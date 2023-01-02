54a1efbd0cea0c2df13aac3d77efc261758b5025
Omero - Laptop <iiomero2582ii@gmail.com>
2023-01-02 13:15:19 -0500

    Working form using controlled component input
    Input is linked to state.input, with state.input being the source of
    truth. Now need to handle the submit, which we are currently just
    console.logging. AKA need to implement Overview component, which should
    render all the tasks...
    
    All tasks should could be kept internally as a state in Overview, but
    I think it makes more sense that App.js as a whole knows about allTasks.
    So App.js should probably keep track of this, then pass it as a prop to
    Overview.js. Big Ups

--- src/App.js
+++ src/App.js
@@ -5,22 +5,30 @@ class App extends Component {
     super(props);
     this.state = {
       tasks: [],
+      input: '',
     }
   }
 
-  submitTask(e, input) {
+  inputTask = (e) => {
+    this.setState({
+      input: e.target.value,
+    }); 
+  }
+
+  submitTask = (e) => {
     e.preventDefault();
-    console.log(input);
+    console.log(this.state.input);
   }
 
   render() {
     return (
       <div className="App">
-        <form onSubmit={(e, input) => this.submitTask(e, input)}>
+        <form onSubmit={this.submitTask}>
           <label htmlFor="task">Task:</label>
-          <input name="task" id="task"/>
+          <input onChange={this.inputTask} name="task" id="task"/>
           <button>Submit</button>
         </form>
+        <div className="inputOut">{this.state.input}</div>
         <div className='title'>My Tasks</div>
         <div className="allTasks"></div>
       </div>
